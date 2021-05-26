a344d75d9cfd186e473ef036e3339f34994707a8
Jonas <web@emd.art.br>
2021-05-25 22:45:22 -0300

    correação de classes

--- src/components/CadastroLogin/CadastroLogin.js
+++ src/components/CadastroLogin/CadastroLogin.js
@@ -17,29 +17,31 @@ export default function CadastroLogin() {
 
       <div className = "mainDiv">
         <form className = "form-main">
-          <div class = "row">
-            <div class = "col">
-            <small id = "passwordHelpBlock" class = "form-text text-muted descricao" >Nome</small>
-            <input type = "text" name = "nome" class = "form-control input-menor" placeholder = "Nome"></input>
+          <div className = "row">
+            <div className = "col">
+            <small id = "passwordHelpBlock" className = "form-text text-muted descricao" >Nome</small>
+            <input type = "text" name = "nome" className = "form-control input-menor" placeholder = "Nome"></input>
           </div>        
-          <div class = "col">
-            <small id = "passwordHelpBlock" class = "form-text text-muted descricao">Sobrenome</small>
-            <input type = "text" name = "sobrenome" class = "form-control input-menor" placeholder = "Sobrenome"></input>
+          <div className = "col">
+            <small id = "passwordHelpBlock" className = "form-text text-muted descricao">Sobrenome</small>
+            <input type = "text" name = "sobrenome" className = "form-control input-menor" placeholder = "Sobrenome"></input>
           </div>        
         </div>
-        <small id = "passwordHelpBlock" class = "form-text text-muted descricao">Endereço de e - mail</small>
-        <input type = "email" name = "email" d = "inputPassword5"class = "form-control input-email" aria-describedby = "passwordHelpBlock"></input>
-        <small id = "passwordHelpBlock" class = "form-text text-muted descricao">Senha</small>
+        <small id = "passwordHelpBlock" className = "form-text text-muted descricao">Endereço de e - mail</small>
+        <input type = "email" name = "email" d = "inputPassword5"className = "form-control input-email" aria-describedby = "passwordHelpBlock"></input>
+        <small id = "passwordHelpBlock" className = "form-text text-muted descricao">Senha</small>
         <input type = "password" name = "senha" id = "inputPassword5" class = "form-control input-senha" aria-describedby = "passwordHelpBlock"></input>
-        <small id = "passwordHelpBlock" class = "form-text text-muted descricao">Confirmar senha</small>
-        <input type = "password" id = "inputPassword5" class = "form-control input-comfirmar-senha"aria-describedby = "passwordHelpBlock"></input>
-        <button type = "submit" class = "btn btn-warning button-create">Criar nova conta</button>
-        <button type = "text" class = "btn btn-warning button-account" text-decoration = 'none'><a href = "/login"> á tenho conta</a>
+        <small id = "passwordHelpBlock" className = "form-text text-muted descricao">Confirmar senha</small>
+        <input type = "password" id = "inputPassword5" className = "form-control input-comfirmar-senha"aria-describedby = "passwordHelpBlock"></input>
+        <button type = "submit" className = "btn btn-warning button-create">Criar nova conta</button>
+        <button type = "text" className = "btn btn-warning button-account" text-decoration = 'none'><a href = "/login"> á tenho conta</a>
         </button>
         </form>
       </div>
 
       </div>
         
+    
   )
+
 }
